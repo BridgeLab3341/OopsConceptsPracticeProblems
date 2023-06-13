e5e42e320b255f83c1df24432580fd0eff8c8678
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-13 17:59:22 +0530

    [somu]Add:Inheritance Concepts With Example.

--- OopsConceptsPracticeProblems/OopsOperations.cs
+++ OopsConceptsPracticeProblems/OopsOperations.cs
@@ -13,4 +13,62 @@ namespace OopsConceptsPracticeProblems
             Console.WriteLine("Hello Good Morning Guys");
         }
     }
+    public class Animal //Single Inheritance
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Single Inheritance");
+            Console.WriteLine("================== ");
+            Console.WriteLine("Animal is eating");
+        }
+    }
+    public class Dog:Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking");
+        }
+    }
+
+    //Multi-level inheritance
+    public class Mammal:Animal
+    {
+        public void Run()
+        {
+            Console.WriteLine("Multi-level Inheritance");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Mammal is running");
+        }
+    }
+    public class Horse:Mammal
+    {
+        public void Gallop()
+        {
+            Console.WriteLine("Horse is galloping");
+        }
+    }
+    // hierarchical inheritance
+    public class Bird:Animal
+    {
+        public void Fly()
+        {
+            Console.WriteLine("hierarchical Inheritance");
+            Console.WriteLine("=========================");
+            Console.WriteLine("Bird is flying.");
+        }
+    }
+    public class Eagle:Bird
+    {
+        public void Hunt()
+        {
+            Console.WriteLine("Eagle is hunting");
+        }
+    }
+    public class Penguin:Bird
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Penguin is swimming");
+        }
+    }
 }
--- OopsConceptsPracticeProblems/Program.cs
+++ OopsConceptsPracticeProblems/Program.cs
@@ -4,8 +4,40 @@
     {
         public static void Main(string[] args)
         {
-            OopsOperations oopsOperations = new OopsOperations();
-            oopsOperations.ConceptObjectClass();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choos Option To Perform\n1.Object and Class Example\n2.Single Inheritance\n3.Multi-Level Inheritance\n4.Hierarchical Inheritance\n5.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        OopsOperations oopsOperations = new OopsOperations();
+                        oopsOperations.ConceptObjectClass();
+                        break;
+                        case 2:
+                        Dog dog= new Dog();
+                        dog.Eat();
+                        dog.Bark();
+                        break;
+                        case 3:
+                        Horse horse= new Horse();
+                        horse.Run();
+                        horse.Gallop();
+                        break;
+                        case 4:
+                        Penguin penguin= new Penguin();
+                        Eagle eagle= new Eagle();
+                        eagle.Fly();
+                        eagle.Hunt();
+                        penguin.Fly();
+                        penguin.Swim();
+                        break;
+                        case 5:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
