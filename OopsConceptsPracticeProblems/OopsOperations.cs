78fd5da9442d9e7dd37754a015becb35cb681b60
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-13 13:15:57 +0530

    [somu]Add:Creating Program With Object and Class Concepts With Example.

---
+++ OopsConceptsPracticeProblems/OopsOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConceptsPracticeProblems
+{
+    public class OopsOperations
+    {
+        public void ConceptObjectClass()
+        {
+            Console.WriteLine("Hello Good Morning Guys");
+        }
+    }
+}
---
+++ OopsConceptsPracticeProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OopsConceptsPracticeProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            OopsOperations oopsOperations = new OopsOperations();
+            oopsOperations.ConceptObjectClass();
+        }
+    }
+}
